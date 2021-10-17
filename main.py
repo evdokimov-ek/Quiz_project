474fba819a507b194b77488bbd7d2562cabaa384
Evgeniy Evdokimov <evdokimov.ek@gmail.com>
2021-10-17 22:11:26 +0300

    add structure

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+class User:
+
+    def __init__(self, user_id, user_name):
+        self.id = user_id
+        self.name = user_name
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
-def print_hi(name):
-    print("Hi, {0}".format(name))
-
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
+user_1 = User(1, "Bob")
+print(user_1.id)
